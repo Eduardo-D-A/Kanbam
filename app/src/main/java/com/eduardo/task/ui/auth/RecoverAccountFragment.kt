80104704a7eed51994508a6e39ea2bdac9770878
Eduardo-D-A <eduardodiasamaral@gmail.com>
2025-06-26 05:07:36 -0300

    Kanbam V5

--- app/src/main/java/com/eduardo/task/ui/auth/RecoverAccountFragment.kt
+++ app/src/main/java/com/eduardo/task/ui/auth/RecoverAccountFragment.kt
@@ -27,7 +27,6 @@ class RecoverAccountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initToolbar(binding.toolBar)
         initListener()
     }
 
